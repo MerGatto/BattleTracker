3441cd3e4a1c2f65d497a801de725502cedd7069
angular-cli <angular-cli@angular.io>
2017-02-11 11:40:15 +0100

    chore: initial commit from angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { BattleTrackerPage } from './app.po';
+
+describe('battle-tracker App', function() {
+  let page: BattleTrackerPage;
+
+  beforeEach(() => {
+    page = new BattleTrackerPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
